789a403fa2350d8688a63563d62a86e490f59133
Serhii Kondratskyi <Blackdato@gmail.com>
2020-05-21 19:54:59 +0300

    finall

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-// import "@babel/polyfill";
-// import 'nodelist-foreach-polyfill';
-// import "es6-promise";
-// // import "fetch-polyfill";
-// import elementClosest from 'element-closest';
-// elementClosest(window);
-// require('formdata-polyfill');
+import "@babel/polyfill";
+import 'nodelist-foreach-polyfill';
+import "es6-promise";
+import "fetch-polyfill";
+import elementClosest from 'element-closest';
+elementClosest(window);
+require('formdata-polyfill');
 // import 'whatwg-fetch';
 
 
